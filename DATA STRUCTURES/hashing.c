7b7c4e99877ea139eb03d157b74f7a3a2e70961c
athulkrishnapulari-sudo <athulkrishnapulari@gmail.com>
2026-09-13 22:40:54 +0530

    python program

---
+++ DATA STRUCTURES/hashing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int size=10;
+
+int MidSquare(int array[size],int size){
+    int ele;
+    printf("Enter The element to Insert");
+    scanf("%d",&ele);
+    int h=ele%size;
+}
+
+int main(){
+
+}
